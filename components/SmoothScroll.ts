e3522ea32c188191dc9017c0db62939b37fecabe
CodeBucks <codebucks27@gmail.com>
2023-06-13 22:39:56 +0530

    issued fixed by using addListener() method

--- components/SmoothScroll.ts
+++ components/SmoothScroll.ts
@@ -1,31 +1,45 @@
-"use client"
-import Scrollbar from 'smooth-scrollbar';
-import { useEffect } from 'react';
-import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
+"use client";
+import Scrollbar from "smooth-scrollbar";
+import { useEffect } from "react";
+import OverscrollPlugin from "smooth-scrollbar/plugins/overscroll";
 
 const overscrollOptions = {
-    enable: true,
-    effect: 'glow',
-    damping: 0.15,
-    maxOverscroll: 150,
-    glowColor: '#E5E5E5',
+  enable: true,
+  effect: "glow",
+  damping: 0.15,
+  maxOverscroll: 150,
+  glowColor: "#E5E5E5",
 };
 
 const options = {
-    damping : 0.07,
-    plugins: {
-        overscroll: { ...overscrollOptions },
-    },
-}
+  damping: 0.07,
+  plugins: {
+    overscroll: { ...overscrollOptions },
+  },
+};
 
 const Scroll = () => {
-    useEffect(() => {
-        Scrollbar.use(OverscrollPlugin);
-        Scrollbar.init(document.body, options);
-        return () => {
-            if (Scrollbar) Scrollbar.destroy(document.body)
-        }  },[])
-    return null;
-}
+  useEffect(() => {
+    Scrollbar.use(OverscrollPlugin);
+
+    const smoothScrollbar = Scrollbar.init(document.body, options);
+
+    smoothScrollbar.addListener((status) => {
+      const headerElement = document.getElementById("header"); // make sure you set the id at the header
+      if (headerElement) {
+        if (status.offset.y > 0) {
+          headerElement.style.top = smoothScrollbar.scrollTop + "px";
+        } else {
+          headerElement.style.top = "auto";
+        }
+      }
+    });
+
+    return () => {
+      if (Scrollbar) Scrollbar.destroy(document.body);
+    };
+  }, []);
+  return null;
+};
 
 export default Scroll;
